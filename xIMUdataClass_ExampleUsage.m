c45db46445c3ab154f6c2c241db5202d95fbf3e4
xioTechnologies <social@x-io.co.uk>
2013-05-27 21:53:55 +0100

    v3.2 commit

--- xIMUdataClass_ExampleUsage.m
+++ xIMUdataClass_ExampleUsage.m
@@ -1,15 +1,21 @@
 %% xIMUdataClass_ExampleUsage.m
-% MATLAB x-IMU Library Version 3.0
+% MATLAB x-IMU Library Version 3.2
 
 addpath('xIMUclasses');     % include class library
 close all;                  % close all figures
 clear;                      % clear all variables
 clc;                        % clear the command terminal
 
-%% Basic usage
+%% Import and plot data
 
-xIMUdata = xIMUdataClass('exampleData\exampleData', 'InertialMagneticSampleRate', 256)
+xIMUdata = xIMUdataClass('exampleData\00000', 'InertialMagneticSampleRate', 256);
 xIMUdata.Plot();
 
-%% End of script
+%% Use magnetometer data to calculate Earth's magnetic field strength
 
+x = xIMUdata.CalInertialMagneticData.Magnetometer.X;
+y = xIMUdata.CalInertialMagneticData.Magnetometer.Y;
+z = xIMUdata.CalInertialMagneticData.Magnetometer.Z;
+disp(sprintf('Earth''s magnetic field strength: %f G', mean(sqrt(x.^2 + y.^2 + z.^2))));
+
+%% End of script
