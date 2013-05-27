a56b9114168cce0a6ca491058b61517b05f2e1a1
xioTechnologies <social@x-io.co.uk>
2013-05-27 21:52:19 +0100

    v2.1 commit

--- xIMUdataClass_ExampleUsage.m
+++ xIMUdataClass_ExampleUsage.m
@@ -1,5 +1,5 @@
 %% xIMUdataClass_ExampleUsage.m
-% MATLAB x-IMU Library Version 2.0
+% MATLAB x-IMU Library Version 2.1
 
 addpath('xIMUclasses');     % include class library
 close all;                  % close all figures
@@ -8,7 +8,7 @@ clc;                        % clear the command terminal
 
 %% Basic usage
 
-xIMUdata = xIMUdataClass('exampleData\exampleData')
+xIMUdata = xIMUdataClass('exampleData\exampleData', 'InertialMagneticSampleRate', 256)
 xIMUdata.Plot();
 
 %% End of script
